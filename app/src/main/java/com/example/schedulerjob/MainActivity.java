df44b99392acc94f97bc22ab491c1402d5e640a1
truongntITS <truongnt@iceteasoftware.com>
2024-12-25 20:35:45 +0700

    update: Jobscheduler

--- app/src/main/java/com/example/schedulerjob/MainActivity.java
+++ app/src/main/java/com/example/schedulerjob/MainActivity.java
@@ -2,32 +2,58 @@ package com.example.schedulerjob;
 
 import static android.content.ContentValues.TAG;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.location.Location;
+import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+
+import helpers.Weather;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static final int[] JOB_IDS = {123, 234, 345};
-    protected static final int[] ALARM_IDS = {101, 102, 103};
-    protected static final long[] INTERVALS = {15 * 60 * 1000, 20 * 60 * 1000, 25 * 60 * 1000}; // 15, 20, 25 minutes
+    private static final int JOB_IDS = 123456;
+    protected static final long INTERVALS = 15 * 60 * 1000; // 15, 20, 25 minutes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,34 +70,47 @@ public class MainActivity extends AppCompatActivity {
         Button startButton = findViewById(R.id.StartSchedulerJob);
         Button stopButton = findViewById(R.id.StopSchedulerJob);
 
-        Button startButtonAlarm = findViewById(R.id.startSchedulerAlarm);
-        Button stopButtonAlarm = findViewById(R.id.stopSchedulerAlarm);
 
         // Gắn sự kiện cho các nút
         startButton.setOnClickListener(view -> {
-            for (int i = 0; i < JOB_IDS.length; i++) {
-                onClickStartScheduler(JOB_IDS[i], INTERVALS[i]);
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            } else {
+                onClickStartScheduler(JOB_IDS, INTERVALS);
+
             }
         });
 
         stopButton.setOnClickListener(view -> {
-            for (int jobId : JOB_IDS) {
-                onClickCancelScheduler(jobId);
-            }
+
+                onClickCancelScheduler(JOB_IDS);
+
         });
 
-        startButtonAlarm.setOnClickListener(view -> {
-            for (int i = 0; i < ALARM_IDS.length; i++) {
-                onClickStartSchedulerAlarm(ALARM_IDS[i], INTERVALS[i]);
-            }
-        });
+        LocalBroadcastManager.getInstance(this).registerReceiver(weatherDataReceiver,
+                new IntentFilter("WEATHER_DATA_ACTION"));
+    }
 
-        stopButtonAlarm.setOnClickListener(view -> {
-            for (int jobId : ALARM_IDS) {
-                onClickCancelSchedulerAlarm(jobId);
-            }
-        });
+    private final BroadcastReceiver weatherDataReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String weatherDescription = intent.getStringExtra("weather_description");
+            double temperature = intent.getDoubleExtra("temperature", 0.0);
+            String cityName = intent.getStringExtra("city_name");
 
+            // Cập nhật UI
+            updateWeatherUI(weatherDescription, temperature, cityName);
+        }
+    };
+
+    private void updateWeatherUI(String weatherDescription, double temperature, String cityName) {
+        TextView weatherInfoTextView = findViewById(R.id.weatherInfoTextView);
+
+        String weatherInfo = "City: " + cityName + "\n"
+                + "Temperature: " + temperature + "°C\n"
+                + "Weather: " + weatherDescription;
+
+        weatherInfoTextView.setText(weatherInfo);
     }
 
 
@@ -81,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
             JobInfo jobInfo = new JobInfo.Builder(jobId, componentName)
                     .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED) // Yêu cầu mạng Wi-Fi
                     .setPersisted(true) // Công việc tồn tại qua reboot
-                    .setPeriodic(intervalMillis, intervalMillis/3) // Chạy định kỳ mỗi 15 phút
+                    .setPeriodic(intervalMillis) // Chạy định kỳ mỗi 15 phút
                     .build();
 
             JobScheduler jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
@@ -100,25 +139,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void onClickStartSchedulerAlarm(int alarmId, long intervalMillis) {
-        AlarmSchedule.scheduleExactAlarm(this, alarmId, intervalMillis);
-    }
-
-
-
-    private void onClickCancelSchedulerAlarm(int alarmId) {
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-
-        Intent intent = new Intent(this, AlarmReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                this, alarmId, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
-
-        if (alarmManager != null) {
-            alarmManager.cancel(pendingIntent);
-            Toast.makeText(this, "Alarm " + alarmId + " cancelled", Toast.LENGTH_SHORT).show();
-            Log.d(TAG, "Alarm " + alarmId + " cancelled");
-        }
-    }
     private void onClickCancelScheduler(int jobId) {
         Toast.makeText(MainActivity.this, "Job " + jobId + " cancelled", Toast.LENGTH_SHORT).show();
 
--- app/src/main/java/com/example/schedulerjob/MyjobService.java
+++ app/src/main/java/com/example/schedulerjob/MyjobService.java
@@ -1,23 +1,49 @@
 package com.example.schedulerjob;
 
+import android.Manifest;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 
+import helpers.Weather;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MyjobService extends JobService {
+
+    Weather weather = new Weather();
+
     public static final String TAG = MyjobService.class.getName();
     private boolean jobCancelled;
-
+    private FusedLocationProviderClient fusedLocationProviderClient;
     @Override
     public boolean onStartJob(JobParameters jobParameters){
-
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
         Log.e(TAG, "Job " + jobParameters.getJobId() + " started");
         doBackgroundWork(jobParameters);
         return true;
@@ -31,6 +57,7 @@ public class MyjobService extends JobService {
                 String currentTime = sdf.format(new Date()); // Lấy thời gian hiện tại
                 Log.e(TAG, "Job " + jobParameters.getJobId() + " - Current Time: " + currentTime);
                 Log.e(TAG, "Job " + jobParameters.getJobId() + " finished");
+                getCurrentLocation();
                 jobFinished(jobParameters, jobCancelled);
             }
         }).start();
@@ -40,6 +67,90 @@ public class MyjobService extends JobService {
         Log.e(TAG, "Job " + jobParameters.getJobId() + " cancelled");
         jobCancelled= true;
         return true;
+    }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Nếu chưa được cấp quyền, thoát ra hoặc yêu cầu quyền
+            Log.e(TAG, "Location permission not granted");
+            return;
+        }
+
+        // Tạo yêu cầu vị trí
+        com.google.android.gms.location.LocationRequest locationRequest = com.google.android.gms.location.LocationRequest.create()
+                .setPriority(com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(15*60*1000) // Cập nhật mỗi 10 giây
+                .setFastestInterval(5000); // Tối thiểu 5 giây
+
+        // Đăng ký cập nhật vị trí
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, new com.google.android.gms.location.LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull com.google.android.gms.location.LocationResult locationResult) {
+                Location location = locationResult.getLastLocation();
+                if (location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    fetchWeatherData(latitude, longitude);
+                    Log.d(TAG, "Updated Latitude: " + latitude + ", Longitude: " + longitude);
+                } else {
+                    Log.d(TAG, "Location is null");
+                }
+            }
+        }, this.getMainLooper());
+    }
+
+    private void sendWeatherDataToUI(Context context, String weatherDescription, double temperature, String cityName) {
+        Intent localIntent = new Intent("WEATHER_DATA_ACTION");
+        localIntent.putExtra("weather_description", weatherDescription);
+        localIntent.putExtra("temperature", temperature);
+        localIntent.putExtra("city_name", cityName);
+
+        LocalBroadcastManager.getInstance(context).sendBroadcast(localIntent);
+    }
+
+    public void fetchWeatherData(double latitude, double longitude) {
+        OkHttpClient client = new OkHttpClient();
+
+        // Đảm bảo rằng `weather.fetchWeatherData()` trả về một Request hợp lệ
+        Request weatherRequest = weather.fetchWeatherData(latitude, longitude);
+
+        client.newCall(weatherRequest).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                if (response.isSuccessful()) {
+                    try {
+                        String responseBody = response.body().string();
+                        JSONObject data = new JSONObject(responseBody); // Phân tích cú pháp JSON từ chuỗi
+                        Log.d("WeatherAPI", "Response JSON: " + data.toString());
+
+                        // Trích xuất thông tin từ JSON
+                        String weatherDescription = data.getJSONArray("weather")
+                                .getJSONObject(0)
+                                .getString("description");
+                        double temperature = data.getJSONObject("main").getDouble("temp");
+                        String cityName = data.getString("name");
+
+                        // Log thông tin
+                        Log.d("WeatherAPI", "City: " + cityName);
+                        Log.d("WeatherAPI", "Temperature: " + temperature + "°C");
+                        Log.d("WeatherAPI", "Weather: " + weatherDescription);
+
+                        sendWeatherDataToUI(MyjobService.this, weatherDescription, temperature, cityName);
+                    } catch (IOException | JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Log.d(TAG, "Failed to fetch weather data.");
+
+                }
+            }
+        });
     }
 
 
